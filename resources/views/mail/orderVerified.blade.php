02f0d7f96743effb4814da42472e9f4affa873a6
sundevildi <sundevildi@gmail.com>
2024-07-02 13:09:50 +0200

    fixed mail table

--- resources/views/mail/orderVerified.blade.php
+++ resources/views/mail/orderVerified.blade.php
@@ -7,96 +7,87 @@
 <body>
 
 <div style="font-size: 20px;">
-    <table width="100%">
+    <table width="100%" border="1px solid #999" cellpadding="5" cellspacing="1" bgcolor="#f5f5f5" style="margin-bottom: 10px;">
         <tr>
-            <td colspan="3" style="text-align: center">
+            <td colspan="2" style="text-align: center">
                 <a href="https://polandgroups.pl/shop/">
                     <img src="https://polandgroups.pl/shop/img/logo.9332798f.png" alt="logo">
                 </a>
             </td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td colspan="2">
                 <div>Dzień dobry {{ empty($order->alt_deliver_information) ? $order->deliver_information['fullname'] : $order->alt_deliver_information['fullname'] }}</div>
             </td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td colspan=2">
                 <div>Twój zakup</div>
                 <div>z dnia {{ date('d.m.Y', strtotime('now')) }}</div>
             </td>
         </tr>
         <tr>
             @foreach($order->items as $item)
-            <td width="60%">{{$item->product->name}}</td>
+            <td>{{$item->product->name}}</td>
             @endforeach
-            <td width="10%">--</td>
-            <td width="30">{{$order->getClearPrice()}} zł</td>
+            <td width="80%">{{$order->getClearPrice()}} zł</td>
         </tr>
         <tr>
             <td>Kurier DPD, <strong>(Ulica dom klienta) Zagrodowa 3</strong></td>
-            <td>--</td>
             <td>
                 <div>{{$order->getBoxPrice()}} zł</div>
             </td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
             <td>
-                <div>__________</div>
                 <div>RAZEM {{$order->getPrice()}} zł</div>
             </td>
         </tr>
+    </table>
+
+    <table width="100%" border="1px solid #999" cellpadding="5" cellspacing="1" bgcolor="#f5f5f5" style="margin-bottom: 10px;>
         <tr>
             <td colspan="3">Przewidywany czas dostawy {{ date('d.m.Y', strtotime("+3 day")) }} u Ciebie</td>
         </tr>
     </table>
 
-    <table width="100%" style="margin-top: 50px;">
+    <table width="100%" border="1px solid #999" cellpadding="5" cellspacing="1" bgcolor="#f5f5f5" style="margin-bottom: 10px;">
         <tr>
-            <td width="60%">Adres e-mail</td>
-            <td width="10%">--</td>
-            <td width="30">{{$order->user_information['email']}}</td>
+            <td>Adres e-mail</td>
+            <td width="80%">{{$order->user_information['email']}}</td>
         </tr>
         <tr>
             <td>Firma</td>
-            <td>--</td>
             <td>{{$order->deliver_information['business']}}</td>
         </tr>
         <tr>
             <td>NIP UE</td>
-            <td>--</td>
             <td>{{$order->deliver_information['nip_ue']}}</td>
         </tr>
         <tr>
             <td>Adress</td>
-            <td>--</td>
             <td>{{$order->deliver_information['address']}}</td>
         </tr>
         <tr>
             <td>Kod pocztowy</td>
-            <td>--</td>
             <td>{{$order->deliver_information['zip_code']}}</td>
         </tr>
         <tr>
             <td>Country</td>
-            <td>--</td>
             <td>{{$order->deliver_information['country']}}</td>
         </tr>
         <tr>
             <td>Miasto</td>
-            <td>--</td>
             <td>{{$order->deliver_information['city']}}</td>
         </tr>
         <tr>
             <td>Telefoniczny numer kontaktowy</td>
-            <td>--</td>
             <td>{{$order->deliver_information['phone']}}</td>
         </tr>
     </table>
     @if (!empty($order->alt_deliver_information))
-    <table style="margin-top: 50px" width="100%">
+    <table width="100%" border="1px solid #999" cellpadding="5" cellspacing="1" bgcolor="#f5f5f5" style="margin-bottom: 10px;">
         <tr>
             <td width="60%">Proszę użyć innego adresu rozliczeniowego</td>
             <td width="10%">--</td>
@@ -104,42 +95,34 @@
         </tr>
         <tr>
             <td>Imię</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['fullname']}}</td>
         </tr>
         <tr>
             <td>Firma</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['business']}}</td>
         </tr>
         <tr>
             <td>NIP UE</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['nip_ue']}}</td>
         </tr>
         <tr>
             <td>Adress</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['address']}}</td>
         </tr>
         <tr>
             <td>Kod pocztowy</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['zip_code']}}</td>
         </tr>
         <tr>
             <td>Country</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['country']}}</td>
         </tr>
         <tr>
             <td>Miasto</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['city']}}</td>
         </tr>
         <tr>
             <td>Telefoniczny numer kontaktowy</td>
-            <td>--</td>
             <td width="30">{{$order->alt_deliver_information['phone']}}</td>
         </tr>
     </table>
