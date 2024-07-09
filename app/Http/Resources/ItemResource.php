c1ed269faf5c56ca5276c5ebbac3061ffe9cfc73
USER-PC\Professional <topup@mail>
2024-07-09 16:35:46 +0300

    Исправление стоимости доставки

--- app/Http/Resources/ItemResource.php
+++ app/Http/Resources/ItemResource.php
@@ -12,13 +12,13 @@ class ItemResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'product' => ProductInOrderResource::make($this->whenLoaded('product')),
-            'product_id' => $this->product_id,
-            'count' => $this->count,
-            'box_count' => $this->boxCount(),
-            'box_price' => round($this->product->box->price, 2),
-            'box_total_price' => $this->boxPrice(),
-            'price' => $this->getPrice(),
+            'product'           => ProductInOrderResource::make($this->whenLoaded('product')),
+            'product_id'        => $this->product_id,
+            'count'             => $this->count,
+            'box_count'         => $this->boxCount(),
+            'box_price'         => round($this->product->box->price, 2),
+            'box_total_price'   => $this->boxPrice(),
+            'price'             => $this->getPrice(),
         ];
     }
 }
